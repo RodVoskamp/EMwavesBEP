bf7be26fef0c4e403e8e86d99934fe68134396c5
RodVoskamp <R.Voskamp@student.tudelft.nl>
2019-10-28 17:15:08 +0100

    mesh part 2

--- src/load_gmsh.jl
+++ src/load_gmsh.jl
@@ -5,5 +5,10 @@ using EMwavesBEP
 using CompScienceMeshes
 
 d = dirname(pathof(EMwavesBEP))
+
 include(joinpath(d,"gmsh3d.jl"))
-Γ = read_gmsh3d_mesh(joinpath(d, "smallfemsphere.msh"))
+Γ = read_gmsh3d_mesh(joinpath(d,"smallfemsphere.msh"))
+
+#d2 = dirname(pathof(CompScienceMeshes))
+#include(joinpath(d2,"fileio/gmsh.jl"))
+#Γ = read_gmsh_mesh(joinpath(d,"smallmomsphere.msh"))
